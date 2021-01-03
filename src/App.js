0e9592dffcd2d8a73f7b02f2763a086834a52c6c
Thang Cao <shiyoline@gmail.com>
2021-01-03 11:07:11 -0800

    display a random anecdote from a click of a button

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import React, { useState } from 'react'
 
-const App = () => {
-  return <div></div>
+// @return random int from 0 to max - 1
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max))
+}
+
+const App = props => {
+  const [selected, setSelected] = useState(0)
+  console.log(props.anecdotes.length)
+  return (
+    <div>
+      <div>{props.anecdotes[selected]}</div>
+      <div>
+        <button
+          onClick={() => setSelected(getRandomInt(props.anecdotes.length))}>
+          next
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default App
