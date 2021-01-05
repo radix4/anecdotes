32b2f017420f90ab136faf7678756ce5fff42987
Thang Cao <shiyoline@gmail.com>
2021-01-04 19:16:46 -0800

    add vote functionality

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import React, { useState } from 'react'
 
-// @return random int from 0 to max - 1
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max))
-}
-
 const App = props => {
   const [selected, setSelected] = useState(0)
-  console.log(props.anecdotes.length)
+  const [votes, setVotes] = useState(Array(props.anecdotes.length).fill(0)) //creates an array with 0s filled
+
+  console.log('array length: ', props.anecdotes.length)
+  console.log('array: ', votes)
+
+  // return a random number from 0 to array length
+  const randomNumber = () =>
+    Math.floor(Math.random() * Math.floor(props.anecdotes.length))
+
+  const handleVote = () => {
+    const copy = { ...votes }
+    copy[selected] += 1
+
+    return setVotes(copy)
+  }
+
   return (
     <div>
       <div>{props.anecdotes[selected]}</div>
+      <div>selected = {selected}</div>
+      <div>has {votes[selected]} votes</div>
       <div>
-        <button
-          onClick={() => setSelected(getRandomInt(props.anecdotes.length))}>
-          next
-        </button>
+        <button onClick={handleVote}>vote</button>
+        <button onClick={() => setSelected(randomNumber)}>next anecdote</button>
       </div>
     </div>
   )
